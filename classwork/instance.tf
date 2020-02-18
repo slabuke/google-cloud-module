6a10e2cfed7d71da97dded58364c0ba3322cd04c
Vitaly Andrushkevich <46499807+Vitalyazavr@users.noreply.github.com>
2020-02-18 09:36:13 +0300

    Add files via upload

---
+++ classwork/instance.tf
@@ -0,0 +1,37 @@
+provider "google" {
+  credentials = "${file("~/gcp/.terraform-admin.json")}"
+  project     = "${var.project}"
+  region      = "${var.region}"
+  zone        = "${var.region}"
+}
+
+resource "google_compute_instance" "nginx-classwork-terraform" {
+  count               = "${var.instances}"
+  name                = "${var.name}-${count.index}"
+  zone                = "${var.zone}"
+  machine_type        = "${var.type}"
+  deletion_protection = "${var.deletion}"
+
+  boot_disk {
+    initialize_params {
+    size  = "${var.disk_size}"
+    type  = "${var.disk_type}"
+    image = "${var.disk_image}"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    subnetwork    = "default"
+    access_config = {}
+  }
+
+labels {
+  servertype        = "nginxserver"
+  osfamily          = "debian"
+  wayofinstallation = "terraform"
+}
+
+metadata_startup_script = "sudo apt-get -y update; sudo apt-get install nginx; sudo systemctl start nginx; sudo systemctl enable nginx"
+
+}
---
+++ classwork/variables.tf
@@ -0,0 +1,71 @@
+/*output "http" {
+ // value = "http://${google_compute_instance.nginx-classwork-terraform.network_interface.0.access_config.0.nat_ip}"
+}
+*/
+
+
+variable "instances"{
+  default = 2
+}
+
+variable "description" {
+  default = "Train hard"
+}
+
+
+variable "project" {
+  type = "string"
+  description = "Set name of project"
+  default = "glassy-hue-268209"
+}
+
+variable "region" {
+  type = "string"
+  description = "Set region"
+  default = "us-central1"
+}
+
+variable "zone" {
+  type = "string"
+  description = "Set zone"
+  default = "us-central1-a"
+}
+
+variable "name" {
+  type = "string"
+  description = "Set the name of instance"
+  default = "nginx-classwork-terraform"
+}
+
+variable "deletion"{
+  default = true
+}
+
+variable "type" {
+  type = "string"
+  description = "1CPU with 1792 Gb RAM"
+  default = "custom-1-1792"
+}
+
+variable "disk_size" {
+  description = "Size of our drive"
+  default = "30"
+}
+
+variable "disk_type" {
+  type = "string"
+  description = "Type of disk"
+  default = "pd-ssd"
+}
+
+variable "disk_image" {
+  type = "string"
+  description = "Type of disk image"
+  default = "ubuntu-1604-xenial-v20200129"
+}
+
+variable "tags" {
+  type    = "list"
+  default = ["http-server", "https-server"]
+}
+
