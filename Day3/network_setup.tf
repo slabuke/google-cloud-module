281270e65ec9a55b6a79cc0e53f95272f4380b58
Vitaly Andrushkevich <46499807+Vitalyazavr@users.noreply.github.com>
2020-02-18 21:58:01 +0300

    Add files via upload

---
+++ Day3/network_setup.tf
@@ -0,0 +1,64 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "vandrushkevich-vpc"
+  description             = "my network"
+  auto_create_subnetworks = "false"
+  routing_mode            = "GLOBAL"
+}
+
+
+resource "google_compute_firewall" "external-allow" {
+  name        = "vandrushkevich-external"
+  description = "Allow external 80 port"
+  network     = "${google_compute_network.vpc_network.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  target_tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "external-allow2" {
+  name        = "vandrushkevich-external"
+  description = "Allow external 22 port"
+  network     = "${google_compute_network.vpc_network.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  target_tags = ["ssh"]
+}
+
+resource "google_compute_firewall" "allow-internal" {
+  name    = "vandrushekvich-internal"
+  network = "${google_compute_network.vpc_network.name}"
+  
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+    
+  }
+  direction = "EGRESS"
+
+}
+
+resource "google_compute_subnetwork" "subnetpriv" {
+  name          = "vandrushkevich-private"
+  ip_cidr_range = "10.14.2.0/24"
+  region        = "us-central1"
+  network       = "${google_compute_network.vpc_network.name}"
+  description = "private subnetwork"
+}
+
+resource "google_compute_subnetwork" "subnetpub" {
+  name          = "vandrushkevich-public"
+  ip_cidr_range = "10.14.1.0/24"
+  region        = "us-central1"
+  network       = "${google_compute_network.vpc_network.name}"
+  description   = "public subnetwork"
+}
---
+++ Day3/variables.tf
@@ -0,0 +1,66 @@
+
+variable "project" {
+  type = "string"
+  description = "Set name of project"
+  default = "glassy-hue-268209"
+}
+
+variable "region" {
+  type = "string"
+  description = "Set region"
+  default = "us-central1"
+}
+
+variable "zone" {
+  type = "string"
+  description = "Set zone"
+  default = "us-central1-a"
+}
+
+variable "name" {
+  type = "string"
+  description = "Set the name of instance"
+  default = "vandrushkevich"
+}
+
+variable "deletion"{
+  default = true
+}
+
+variable "type" {
+  type = "string"
+  description = "1CPU with 1792 Gb RAM"
+  default = "custom-1-1792"
+}
+
+variable "disk_size" {
+  description = "Size of our drive"
+  default = "30"
+}
+
+variable "disk_type" {
+  type = "string"
+  description = "Type of disk"
+  default = "pd-ssd"
+}
+
+variable "disk_image" {
+  type = "string"
+  description = "Type of disk image"
+  default = "ubuntu-1604-xenial-v20200129"
+}
+
+variable "tags" {
+  type    = "list"
+  default = ["http-server", "https-server"]
+}
+
+variable "nginx_setup"{
+  default = <<EOF
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo echo "<h1>Hello from vandrushkevich!</h1>" > /usr/share/nginx/html/index.html
+sudo systemctl start nginx
+sudo systemctl enable nginx
+EOF
+}
