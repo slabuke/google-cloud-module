c5bdc7a370aad84531f6055160cbb1eaa618946f
student <student@EPBYMINW9093.minsk.epam.com>
2020-02-18 09:56:13 +0300

    v.1.1

--- classwork/output.tf
+++ classwork/output.tf
@@ -1,3 +1,3 @@
-output "link" {
-  value = "http://google_compute_instance.nginx-classwork-terraform.*.network_interface.0.access_config.0.nat_ip"
+output "IPs" {
+  value = "${google_compute_instance.nginx-classwork-terraform.*.network_interface.0.access_config.0.nat_ip }"
 }
