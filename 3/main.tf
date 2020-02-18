535e7319e205834f697747a14fc8e22afbc2f6a9
student <student@localhost.localdomain>
2020-02-18 21:56:22 +0300

    Supercommit

---
+++ 3/main.tf
@@ -0,0 +1,82 @@
+provider "google" {
+  credentials = "${file(".instant-river-268209-57e284821158.json")}"
+  project     = "instant-river-268209"
+  zone        = "us-central1-a"
+}
+
+resource "google_compute_network" "networks" {
+  name                    = "${var.student_name}-vpc"
+  auto_create_subnetworks = false
+  description             = "${var.description}"
+}
+
+resource "google_compute_firewall" "firewalls_private" {
+  name          = "${var.firewall}-private"
+  network       = "${google_compute_network.networks.name}"
+  description   = "${var.description}"
+  source_ranges = ["10.12.1.0/24"]
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+}
+
+resource "google_compute_firewall" "firewalls_public" {
+  name        = "${var.firewall}-public"
+  network     = "${google_compute_network.networks.name}"
+  description = "${var.description}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "22"]
+  }
+}
+
+resource "google_compute_subnetwork" "public_subnetwork" {
+  name          = "${var.pb_subnet}"
+  region        = "${var.region}"
+  network       = "${google_compute_network.networks.self_link}"
+  ip_cidr_range = "${var.ip1}"
+  description   = "${var.description}"
+}
+
+resource "google_compute_subnetwork" "private_subnetwork" {
+  name          = "${var.pr_subnet}"
+  region        = "${var.region}"
+  network       = "${google_compute_network.networks.self_link}"
+  ip_cidr_range = "${var.ip2}"
+  description   = "${var.description}"
+}
+
+resource "google_compute_instance" "network" {
+  name         = "${var.name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.zone}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+      size  = "${var.size}"
+      type  = "${var.type}"
+    }
+  }
+
+  network_interface {
+    network       = "${var.student_name}-vpc"
+    subnetwork    = "${var.pb_subnet}"
+    access_config = {}
+  }
+
+  metadata_startup_script = "${var.script}"
+
+  tags = "${var.tags}"
+}
---
+++ 3/output.tf
@@ -0,0 +1,67 @@
+variable "student_name" {
+  default = "dmitriy"
+}
+
+variable "script" {
+  default = "sudo yum update -y \n sudo yum install nginx -y \n sudo systemctl restart nginx \n sudo echo 'Hello from Dmitriy Shishkovets!' > /tmp/index1.html \n cp /tmp/index1.html /usr/share/nginx/html/index.html"
+}
+
+variable "firewall" {
+  default = "firewall"
+}
+
+variable "pr_subnet" {
+  default = "private-subnetwork"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "description" {
+  default = "Description "
+}
+
+variable "pb_subnet" {
+  default = "public-subnetwork"
+}
+
+variable "ip1" {
+  default = "10.12.1.0/24"
+}
+
+variable "ip2" {
+  default = "10.12.2.0/24"
+}
+
+variable "name" {
+  default = "nginx-terraform"
+}
+
+variable "machine_type" {
+  default = "custom-1-4608"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+variable "image" {
+  default = "centos-7-v20200205"
+}
+
+variable "size" {
+  default = 10
+}
+
+variable "type" {
+  default = "pd-ssd"
+}
+
+variable "network" {
+  default = "default"
+}
+
+variable "tags" {
+  default = ["http-server", "https-server"]
+}
