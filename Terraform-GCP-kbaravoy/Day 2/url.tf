63d9d5c74f1d5ad072447ff6c4b691fe5e077c0f
borovoykirill <borovoy.kirill93@gmail.com>
2020-02-21 13:05:54 +0300

    Delete

--- Terraform-GCP-kbaravoy/Day 2/url.tf
+++
@@ -1,3 +0,0 @@
-output "url" {
-  value = "http://${google_compute_instance.nginx-terraform.network_interface.0.access_config.0.nat_ip}"
-}
--- Terraform-GCP-kbaravoy/cw/url.tf
+++
@@ -1,3 +0,0 @@
-output "url" {
-  value = "${google_compute_instance.v.*.network_interface.0.access_config.0.nat_ip }"
-}p}"
