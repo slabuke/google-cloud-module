5a709eaaffacb9e796657c3cac1e653224532d5d
borovoykirill <borovoy.kirill93@gmail.com>
2020-02-21 13:09:04 +0300

    Delete

--- Terraform-GCP-LB/example-lb/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "load-balancer-ip" {
-  value = "${module.gce-lb-fr.external_ip}"
-}
--- Terraform-GCP-kbaravoy/Day 5/modules/lb/var.tf
+++
@@ -1,4 +0,0 @@
-variable "public_managed_group" {}
-variable "private_managed_group" {}
-variable "network_name" {}
-variable "subnet-2-name" {}
