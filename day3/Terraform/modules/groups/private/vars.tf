478d60983846914c9e7d90a114be2e68b3feaf40
flyboy14 <twiceaday28@gmail.com>
2020-02-23 20:01:27 +0300

    Get rid of harcode

--- day3/Terraform/modules/groups/private/vars.tf
+++ day3/Terraform/modules/groups/private/vars.tf
@@ -1,3 +1,4 @@
+variable "Project" { default = "devopslab2020-gcp-module" }
 variable "Region" { default = "us-central1" }
 variable "GroupName" { default = "nginx-group" }
 variable "template-self_link" {}
--- day3/Terraform/modules/groups/public/output.tf
+++ day3/Terraform/modules/groups/public/output.tf
@@ -1,5 +1,5 @@
 output "instance-group_name" {
-  value = "https://www.googleapis.com/compute/v1/projects/devopslab2020-gcp-module/regions/us-central1/instanceGroups/${google_compute_region_instance_group_manager.igm-web.name}"
+  value = "https://www.googleapis.com/compute/v1/projects/${var.Project}/regions/${var.Region}/instanceGroups/${google_compute_region_instance_group_manager.igm-web.name}"
 }
 
 output "igm-web-self_link" {
